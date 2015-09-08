5daa7ca0124c1fdeab7db827dcef04cfc4bb2511
emtudo <emtudo@gmail.com>
2015-09-08 11:40:44 -0300

    report ok

--- src/Product/Http/Controllers/ProductController.php
+++ src/Product/Http/Controllers/ProductController.php
@@ -2,11 +2,11 @@
 
 namespace ResultSystems\Storehouse\Product\Http\Controllers;
 
+use Illuminate\Http\Request;
 use ResultSystems\Storehouse\Product\Http\Controllers\Controller;
+use ResultSystems\Storehouse\Product\Repositories\ProductRepository;
 use ResultSystems\Storehouse\Product\Services\ProductCreateService;
 use ResultSystems\Storehouse\Product\Services\ProductUpdateService;
-use ResultSystems\Storehouse\Product\Repositories\ProductRepository;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -25,19 +25,21 @@ class ProductController extends Controller
      */
     private $prodRepo;
     /**
-     * Construtor da classe
+     * Construtor da classe.
+     *
      * @param ProductCreateService $prodCreate
      * @param ProductUpdateService $prodUpdate
      */
     public function __construct(ProductCreateservice $prodCreate, ProductUpdateService $prodUpdate, ProductRepository $prodRepo)
     {
-        $this->prodCreate=$prodCreate;
-        $this->prodUpdate=$prodUpdate;
-        $this->prodRepo=$prodRepo;
+        $this->prodCreate = $prodCreate;
+        $this->prodUpdate = $prodUpdate;
+        $this->prodRepo = $prodRepo;
     }
 
     /**
-     * exibir formulário para criar produto
+     * exibir formulário para criar produto.
+     *
      * @return \Illuminate\View\View
      */
     public function create()
@@ -46,8 +48,10 @@ class ProductController extends Controller
     }
 
     /**
-     * cria um novo produto
+     * cria um novo produto.
+     *
      * @param  \Illuminate\Contracts\Http\Request $request
+     *
      * @return \Illuminate\Contracts\Http\Response
      */
     public function store(Request $request)
@@ -62,39 +66,47 @@ class ProductController extends Controller
         return response('xxxx', 422);
         return response(array('message'=>$prod->save()), 422);
 
-*/
+         */
         return $this->prodCreate->store($request->all());
     }
 
     /**
-     * exibir o produto pelo id
+     * exibir o produto pelo id.
+     *
      * @param  int $id
+     *
      * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        $product=$this->prodRepo->findById($id);
+        $product = $this->prodRepo->findById($id);
+
         return view('storehouse-product::show', compact($product));
     }
 
     /**
-     * apaga um produto
+     * apaga um produto.
+     *
      * @param  array|int  $ids
+     *
      * @return \Illuminate\Contracts\Http\Response
      */
     public function destroy($ids)
     {
-        $result=$this->prodRepo->destroy($ids);
-        if ($result>0) {
-            return response()->json(array('message'=>'Produto(s) apagado(s) com sucesso'), 200);
+        $result = $this->prodRepo->destroy($ids);
+        if ($result > 0) {
+            return response()->json(['message' => 'Produto(s) apagado(s) com sucesso'], 200);
         }
-        return response()->json(array('message'=>'Falha ao tentar apagar o(s) produto(s)', 'error'=>true), 422);
+
+        return response()->json(['message' => 'Falha ao tentar apagar o(s) produto(s)', 'error' => true], 422);
     }
 
     /**
-     * edita as informações do produto
-     * @param  integer $id
+     * edita as informações do produto.
+     *
+     * @param  int $id
      * @param  \Illuminate\Contracts\Http\Request $request
+     *
      * @return \Illuminate\Contracts\Http\Response
      */
     public function update($id, Request $request)
@@ -103,8 +115,10 @@ class ProductController extends Controller
     }
 
     /**
-     * faz pesquisa de produtos
+     * faz pesquisa de produtos.
+     *
      * @param  Request $request
+     *
      * @return \Illuminate\Contracts\Http\Response
      */
     public function search(Request $request)
@@ -113,11 +127,23 @@ class ProductController extends Controller
     }
 
     /**
-     * mostra formulário de pesquisa
+     * mostra formulário de pesquisa.
+     *
      * @return \Illuminate\View\View
      */
     public function viewSearch()
     {
-        return view("storehouse-product::product.search");
+        return view('storehouse-product::product.search');
+    }
+
+    /**
+     * relatório de produtos.
+     * @param  int $id
+     *
+     * @return \Illuminate\Contracts\Http\Response
+     */
+    public function report($id)
+    {
+        return response()->json($this->prodRepo->report($id), 200);
     }
 }
